3e71b49f392d8802bd9be7262ea714c1f66d7737
Tom  Lanstone <thomaslanstone@hotmail.co.uk>
2020-06-17 04:25:53 +0100

    Reverted back to before everything broke

--- NoughtsAndCrosses/Program.cs
+++ NoughtsAndCrosses/Program.cs
@@ -33,18 +33,6 @@ namespace NoughtsAndCrosses
             Console.ReadKey();
         }
 
-<<<<<<< HEAD
-        private static void CheckForStaleMate()
-        {
-            if (board[0] != "1"  && board[1] != "2" && board[2] != "3" && board[3] != "4" && board[4] != "5" && board[5] != "6" && board[6] != "7" && board[7] != "8" && board[8] != "9")
-            {
-                CheckForStaleMate = true;
-            }
-            
-        }
-
-=======
->>>>>>> parent of 7970217... Implemented game ending at stalemate
         private static void DisplayWinner()
         {
             if (winnerShape == "X")
