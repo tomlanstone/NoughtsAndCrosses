7970217362dabb217c12c22fa9dcb7903fd141aa
Tom Lanstone <thomaslanstone@hotmail.co.uk>
2020-06-17 03:32:47 +0100

    Implemented game ending at stalemate

--- NoughtsAndCrosses/Program.cs
+++ NoughtsAndCrosses/Program.cs
@@ -12,11 +12,14 @@ namespace NoughtsAndCrosses
 
         private static bool victory = false;
 
+        private static bool staleMate = false;
+
         private static string player1 = "Player 1";
         
         private static string player2 = "Player 2";
 
         private static string winnerShape = "";
+
         static void Main()
         {
             GetPlayerNames();
@@ -25,6 +28,7 @@ namespace NoughtsAndCrosses
                 MakeBoard();
                 ProcessTurn();
                 CheckForVictory();
+                CheckForStaleMate();
                 TogglePlayer();
             }
 
@@ -33,15 +37,31 @@ namespace NoughtsAndCrosses
             Console.ReadKey();
         }
 
+        private static void CheckForStaleMate()
+        {
+            if (board[0] !=1  && board[1] !=2 && board[2] !=3 && board[3] !=4 && board[4] !=5 && board[5] !=6 && board[6] !=7 && board[7] !=8 && board[8] !=9)
+            {
+                CheckForStaleMate = true;
+            }
+            
+        }
+
         private static void DisplayWinner()
         {
-            if (winnerShape == "X")
+            if (staleMate == false)
             {
-                Console.WriteLine("Congratulations " + player1 + "," + EOL + "You Are The Winner!");
+                if (winnerShape == "X")
+                {
+                    Console.WriteLine("Congratulations " + player1 + "," + EOL + "You Are The Winner!");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations " + player2 + "," + EOL + "You Are The Winner!");
+                }
             }
             else
             {
-                Console.WriteLine("Congratulations " + player2 + "," + EOL + "You Are The Winner!");
+                Console.WriteLine("Game Over" + EOL + "No Winners This Time");
             }
         }
 
